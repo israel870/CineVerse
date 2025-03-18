c037f6e9a1874da3706dca6dd57e662557c8d314
israel870 <israeltobiloba24@gmail.com>
2025-03-18 22:01:40 +0000

    comm

--- app.js
+++ app.js
@@ -3,3 +3,9 @@ function openMenu() {
     document.querySelector('.links').classList.remove("close")
     document.querySelector('.links').classList.add("open")
 }
+
+function closeMenu() {
+    console.log('hello word');
+    document.querySelector('.links').classList.add("close")
+    document.querySelector('.links').classList.remove("open")
+}
