219460a528402827db3ecd7c34ab784dd1769b44
israel870 <israeltobiloba24@gmail.com>
2025-03-22 15:44:30 +0000

    commit

--- app.js
+++ app.js
@@ -11,3 +11,19 @@ document.querySelector('.fa-xmark').addEventListener('click', () => {
     document.querySelector('.menu').classList.close('open')
 
 })
+
+// Search Pop-up
+
+document.querySelector('.open-popup').addEventListener('click', () => {
+
+    document.querySelector('.search-popup').classList.add('open')
+    document.querySelector('.search-popup').classList.remove('close')
+
+})
+
+document.querySelector('.close-popup').addEventListener('click', () => {
+
+    document.querySelector('.search-popup').classList.remove('open')
+    document.querySelector('.search-popup').classList.add('close')
+
+})
